43ddc99f4fcc97ccb4fc5e5984a0728c12965c8e
Recap Time Squad Root user <yourfriends@madebythepins.tk>
2021-12-15 15:32:28 +0800

    Syntax issues, anoyone?
    Co-authored-by: Andrei Jiroh Halili <ajhalili2006@gmail.com>

--- startup-mgmt.sh
+++ startup-mgmt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [[ $SPOTIFY_PROXY_API_TOKEN != "" ]];
+if [[ $SPOTIFY_PROXY_API_TOKEN != "" ]]; then
   echo "warning: Proxy API key was in SPOTIFY_PROXY_API_TOKEN instead of SPOTIFY_PROXY_API_KEY,"
   echo "warning: re-exporting value to correct one."
   export SPOTIFY_PROXY_API_KEY=$SPOTIFY_PROXY_API_TOKEN
 fi
+sleep 3
 
 exec /bin/spotify_auth_proxy
